e4dd509cc3a5be46c26640b50bd2cf375a0f951c
Nazmul1582 <nazmulhasan1582@gmail.com>
2024-05-19 06:00:28 +0600

    attribute binding in vue js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
-    data: function() {
-        return {
-            product: "Socks"
-        }
-    }
-})
+  data() {
+    return {
+      product: "Socks",
+      image: "./assets/images/socks_blue.jpg",
+    };
+  },
+});
